8e48e6f246f058efd448ccd815f9bae557dbdaed
Jacinda Zhong <jacindaz@gmail.com>
2015-03-01 15:32:12 -0500

    Adds city show page and partials folder

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,6 +1,13 @@
 class CitiesController < ApplicationController
 
   def index
+    @cities = City.all
+  end
+
+  def show
+    @region = Region.find(params[:region_id])
+    @city = City.find(params[:id])
+    @country = @city.region.country
   end
 
 end
