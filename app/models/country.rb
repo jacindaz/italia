1e7f05145ebdee7a5accce168d5bbe33625186d0
Jacinda Zhong <jacindaz@gmail.com>
2015-02-28 19:00:37 -0500

    Adds associations and validations to country and region

---
+++ app/models/country.rb
@@ -0,0 +1,15 @@
+class Country < ActiveRecord::Base
+  has_many :regions
+
+  validates :native_language_name, presence: true
+  validates_uniqueness_of :native_language_name
+  
+  validates :english_name, presence: true
+  validates :country_website, url: true
+  validates :description, length: {
+    maximum: 400,
+    tokenizer: lambda { |str| str.scan(/\w+/) },
+    too_short: "must have at least %{count} words.",
+    too_long: "must have less than %{count} words."
+  }
+end
---
+++ app/models/region.rb
@@ -0,0 +1,17 @@
+class Region < ActiveRecord::Base
+  has_many :cities
+  belongs_to :country 
+
+  validates :country_id, presence: true, numericality: { only_integer: true }
+  validates :native_language_name, presence: true
+  validates_uniqueness_of :native_language_name
+
+  validates :english_name, presence: true
+  validates :region_website, url: true
+  validates :description, length: {
+    maximum: 400,
+    tokenizer: lambda { |str| str.scan(/\w+/) },
+    too_short: "must have at least %{count} words.",
+    too_long: "must have less than %{count} words."
+  }
+end
