baf463b73d05cca6e25a605ab0161ab4bd06624f
Jacinda <jacindaz@gmail.com>
2015-03-11 08:08:34 -0400

    Adds form to create a new destination, adds methods to model

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,6 +1,12 @@
 class Destination < ActiveRecord::Base
+  CATEGORIES = ["museum", "church", "historic building", "park", "garden", "castle", "archaelogical sight", "historic street", "historic square", "store"]
+
   validates :name, presence: true
-  validates :category, presence: true, inclusion: { in: ["museum", "church", "historic building", "park", "garden", "castle", "archaelogical sight", "historic street", "historic square", "store"] }
+  validates :category, presence: true, inclusion: { in: CATEGORIES}
   validates :cost, numericality: { only_integer: true}
+
+  def self.categories_for_select
+    Destination::CATEGORIES.map { |category| [category.titleize, category] }
+  end
 
 end
