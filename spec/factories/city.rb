d77175f78e901657051fcbd29d73502ed248f836
Jacinda <jacindaz@gmail.com>
2015-03-13 06:41:49 -0400

    Adds city_with_region_country factory to city model test
     - note that when 2 factories are defined for the same
       Class, you cannot use that factory to define another
       factory
     - remember to re-create attributes using different 
       attribute properties

--- spec/factories/city.rb
+++ spec/factories/city.rb
@@ -8,12 +8,11 @@ FactoryGirl.define do
   end
 
   factory :city_with_region_country, class: City do 
-    sequence(:english_name) { |n| "city english name #{n}"}
-    sequence(:native_language_name) { |n| "city native language name #{n}"}
+    sequence(:english_name) { |n| "city with region country english name #{n}"}
+    sequence(:native_language_name) { |n| "city with region country native language name #{n}"}
     description "blah blah city description blah blah"
     is_capital false
     sequence(:city_website) { |n| "http://www.citytest#{n}.com"}
-    
     association :region, factory: :region_with_country
   end
 end
