ad013a7baa5b5271d15f31fab878ffb1a1a6a21b
Jacinda <jacindaz@gmail.com>
2015-03-15 12:53:01 -0400

    Adds passing test for creating new address + new destination

--- spec/features/destinations/adds_new_destination_spec.rb
+++ spec/features/destinations/adds_new_destination_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 feature 'saving a new destination' do
 
   context "with appropriate factories" do
-    let(:destination_create) { FactoryGirl.create(:destination_with_address) }
     let(:destination) { FactoryGirl.build(:destination_with_address) }
+    let(:destination_no_address) { FactoryGirl.build(:destination_no_address) }
 
     scenario 'user creates a new destination using a pre-existing address' do
       visit new_destination_path(destination)
@@ -33,10 +33,34 @@ feature 'saving a new destination' do
     end
 
     scenario 'user creates a new destination with a new address' do 
+      city = FactoryGirl.create(:city_with_region_country)
+      address = FactoryGirl.build(:address_no_city, city: city)
+
+      visit new_destination_path(:destination_no_address)
+
+      within(".new-destination") do
+        fill_in "Destination Name", with: destination_no_address.name
+        fill_in "Hours", with: destination_no_address.hours
+        fill_in "Website", with: destination_no_address.destination_website
+        fill_in "Description", with: destination_no_address.description
+
+        select destination_no_address.category.titleize, from: "Category"
+        fill_in "Cost", with: destination_no_address.cost
+
+        check "Enter a new Address"
+        within(".new_address") do 
+          fill_in "Street Address", with: address.address
+          fill_in "Phone number", with: address.phone_number 
+
+          select address.city.english_name, from: "Select a City"
+          fill_in "Zipcode", with: address.zip
+          click_on "Save"
+        end
+      end
     end
 
     scenario 'user entering a blank destination should see appropriate errors' do
-      visit new_destination_path(destination)
+      visit new_destination_path(destination_no_address)
       within(".existing-address-submit") do 
         click_on "Save"
       end
