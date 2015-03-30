6cdd382fc4afc05dda619a63835f742bd7e46dab
Jacinda <jacindaz@gmail.com>
2015-03-30 07:07:45 -0400

    Set up destinations#update integration test scenarios

--- spec/features/destinations/updates_existing_destination_spec.rb
+++ spec/features/destinations/updates_existing_destination_spec.rb
@@ -5,6 +5,13 @@ feature 'updating a destination' do
   context "with appropriate factories" do
 
     scenario 'user creates a new destination using a pre-existing address' do
+    scenario 'user updates destination without changes to address' do 
+    end
+
+    scenario 'user only updates address, no changes to destination' do 
+    end
+
+    scenario 'user creates a new address while updating existing destination' do
     end
 
   end
