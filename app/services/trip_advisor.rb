6c4a39bffb8731eea00350a06aee80fc4dc5ff7f
Jacinda Zhong <jacindaz@gmail.com>
2015-05-14 07:24:23 -0400

    It works! Call to TripAdvisor API

--- app/services/trip_advisor.rb
+++ app/services/trip_advisor.rb
@@ -1,12 +1,12 @@
 class TripAdvisor
 	include HTTParty
-	base_uri 'http://api.tripadvisor.com/api//partner/2.0/location/60864'
+	base_uri 'api.tripadvisor.com'
 
 	def initialize
-		@key = ENV["TRIP_ADVISOR_API_KEY"]
+		@options = { "User-Agent" => "Travel_App", "X-TripAdvisor-API-Key" => ENV["TRIP_ADVISOR_API_KEY"] }
 	end
 
 	def attractions
-		self.class.get("/attractions?#{@key}")
+		self.class.get("/api/partner/2.0/location/60864/attractions?#{@key}", headers: @options)
 	end
 end
