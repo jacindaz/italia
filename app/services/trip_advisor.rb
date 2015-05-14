24d10b507d0e7fafedaf9fe2ae8556b3b6b6949e
Jacinda Zhong <jacindaz@gmail.com>
2015-05-14 07:53:07 -0400

    Parses API response into JSON

--- app/services/trip_advisor.rb
+++ app/services/trip_advisor.rb
@@ -2,11 +2,15 @@ class TripAdvisor
 	include HTTParty
 	base_uri 'api.tripadvisor.com'
 
+	LOCATION_IDS = { new_orleans: '60864', italy: '187768', rome: '187791' }
+
 	def initialize
 		@options = { "User-Agent" => "Travel_App", "X-TripAdvisor-API-Key" => ENV["TRIP_ADVISOR_API_KEY"] }
 	end
 
-	def attractions
-		self.class.get("/api/partner/2.0/location/60864/attractions?#{@key}", headers: @options)
+	def attractions(location_name)
+		location_id = LOCATION_IDS[location_name.parameterize.underscore.to_sym]
+		JSON.parse(self.class.get("/api/partner/2.0/location/#{location_id}/attractions?#{@key}", headers: @options).body)["data"]
 	end
 end
+
