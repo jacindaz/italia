26433b4f822790bf88f18a15f7f96fdb0392ed56
Jacinda <jacindaz@gmail.com>
2015-05-15 09:44:01 -0400

    Add method to request destinations in batches of 10
     - API only allows for specifying IDs of 10 destinations
       in 1 call

--- app/services/trip_advisor.rb
+++ app/services/trip_advisor.rb
@@ -18,11 +18,23 @@ class TripAdvisor
 		JSON.parse(self.class.get("/api/partner/2.0/location/#{location_id}/attractions?limit=#{@limit}&offset=#{@offset}", headers: @options).body)["data"]
 	end
 
-	def attraction_by_id
+	def attractions_by_id
 		# http://api.tripadvisor.com/api//partner/2.0/location/210266,638047/attractions?key=5e21e5e014b841b392276ec53ae450c5
+		attractions_id_array_batches = self.attractions_batches_of_10
+		response_batches = []
+
+		attractions_batches_of_10.each do |id_batch|
+			binding.pry
+			id_batch = id_batch.join(",")
+			response_batches << JSON.parse(self.class.get("/api/partner/2.0/location/#{id_batch}/attractions?", headers: @options).body)["data"]
+		end
+		response_batches.flatten.length
+	end
+
+	def attractions_batches_of_10
 		scrape = TripAdvisor.scrape_new_orleans_attractions_no_tours
-		ids = scrape.map{ |a| a[:id] }.reject(&:nil?).join(",")
-		JSON.parse(self.class.get("/api/partner/2.0/location/#{ids}", headers: @options).body)["data"]
+		ids = scrape.map{ |a| a[:id] }.reject(&:nil?).uniq
+		ids.each_slice(10).to_a
 	end
 
 	def self.scrape_new_orleans_attractions_no_tours
@@ -32,10 +44,8 @@ class TripAdvisor
 
 		attractions_link_text = []
 		attractions_div.each do |attraction|
-
 			link = "http://www.tripadvisor.com" + "#{attraction.children[1].attributes["href"].value}"
 			split_destination_link_string = link.split("-d")[1]
-
 			attractions_link_text << { 
 					link: link, 
 					text: attraction.children[1].children[0].text,
@@ -44,15 +54,6 @@ class TripAdvisor
 		end
 
 		attractions_link_text
-
-		# ids = []
-		# attractions_link_text.each do |attraction|
-		# 	split_destination_link_string = attraction[:link].split("-d")[1]
-		# 	if split_destination_link_string 
-		# 		ids << split_destination_link_string.split("-")[0].to_i
-		# 	end
-		# end
-
 	end
 end
 
