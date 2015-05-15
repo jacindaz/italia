c6f29c8cf8217e6ace8bfc6aeabaff4e7549b810
Jacinda Zhong <jacindaz@gmail.com>
2015-05-15 06:57:59 -0400

    Add scraping method, since TripAdvisor API doesn't give back much data
    Lame!!!!

--- app/services/trip_advisor.rb
+++ app/services/trip_advisor.rb
@@ -4,13 +4,33 @@ class TripAdvisor
 
 	LOCATION_IDS = { new_orleans: '60864', italy: '187768', rome: '187791' }
 
-	def initialize
-		@options = { "User-Agent" => "Travel_App", "X-TripAdvisor-API-Key" => ENV["TRIP_ADVISOR_API_KEY"] }
+	def initialize(limit=nil, offset=nil)
+		@options = { 
+								"User-Agent" => "Travel_App", 
+								"X-TripAdvisor-API-Key" => ENV["TRIP_ADVISOR_API_KEY"]
+							}
+		@limit = limit
+		@offset = offset
 	end
 
 	def attractions(location_name)
 		location_id = LOCATION_IDS[location_name.parameterize.underscore.to_sym]
-		JSON.parse(self.class.get("/api/partner/2.0/location/#{location_id}/attractions?#{@key}", headers: @options).body)["data"]
+		JSON.parse(self.class.get("/api/partner/2.0/location/#{location_id}/attractions?limit=#{@limit}&offset=#{@offset}", headers: @options).body)["data"]
+	end
+
+	def self.scrape_new_orleans_top_30_attractions
+		encode_url = URI::encode("http://www.tripadvisor.com/Attractions-g60864-Activities-New_Orleans_Louisiana.html")
+		page = Nokogiri::HTML(open("#{encode_url}"))
+		attractions_div = page.xpath('//div[contains(@class, "property_title")]')
+
+		attractions_link_text = []
+		attractions_div.each do |attraction|
+			attractions_link_text << { 
+					link: "http://www.tripadvisor.com" + "#{attraction.children[1].attributes["href"].value}", 
+					text: attraction.children[1].children[0].text 
+				}
+		end
+
 	end
 end
 
