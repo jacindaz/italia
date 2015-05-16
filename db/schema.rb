fe61116c0dce921f3864a3684502d456dd2b004f
Jacinda Zhong <jacindaz@gmail.com>
2015-05-16 15:05:15 -0400

    Change destination/address association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150325113420) do
+ActiveRecord::Schema.define(version: 20150516185637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 20150325113420) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "zip"
-    t.integer  "destination_id"
   end
 
   create_table "cities", force: :cascade do |t|
@@ -70,6 +69,7 @@ ActiveRecord::Schema.define(version: 20150325113420) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "address_id",                        null: false
   end
 
   create_table "hours", force: :cascade do |t|
